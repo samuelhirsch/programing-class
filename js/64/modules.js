b2331996fbef4cb0a91daf3419fd528a98b2ec76
samuelhirsch <shmielhirsch@gmail.com>
2025-09-01 22:30:15 -0400

    Hw-65

--- js/64/modules.js
+++ js/64/modules.js
@@ -1,39 +1,43 @@
 'use strict';
 
 
-const DAYOFWEEK=(function(){
-const DAYS=['Sunday','Monday','Tuesday','Wednsday','Thursday','Friday','Saturday'];
-return{
- getDayname(number){
- return DAYS[number-1];
-},
- getDaynumber(day){
- return DAYS.findIndex(d=>d===day)+1;
-  }
- };
+const DAYOFWEEK = (function () {
+  const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednsday', 'Thursday', 'Friday', 'Saturday'];
+  return {
+    getDayname(number) {
+      return DAYS[number - 1];
+    },
+    getDaynumber(day) {
+      return DAYS.findIndex(d => d === day) + 1;
+    }
+  };
 }());
 
 console.log(DAYOFWEEK.getDayname(3));
 console.log(DAYOFWEEK.getDaynumber('Friday'));
 
-const CALCULATOR =(function(){
- let interestrate=0;
- let years=0;
- return{
-     setrate(rate){
-      interestrate=rate;
+const CALCULATOR = (function () {
+  let interestrate = 0;
+  let years = 0;
+  return {
+    setrate(rate) {
+      interestrate = rate;
       return this;
-     },
-     setyear(numberofyears){
-       years=numberofyears;
-       return this;
-     },
-     calculateinterest(principal){
-       return principal * interestrate * years; 
-     }
- };
+    },
+    setyear(numberofyears) {
+      years = numberofyears;
+      return this;
+    },
+    calculateinterest(principal) {
+      let amount = principal;
+      for (let i = 0; i < years; i++) {
+        amount +=amount*interestrate;
+      }
+      return amount - principal;
+    }
+  };
 }());
-console.log(CALCULATOR.setrate(0.05).setyear(5).calculateinterest(20000));
+console.log(CALCULATOR.setrate(.1).setyear(2).calculateinterest(100));
 
 
 
---
+++ js/65/hw65.js
@@ -0,0 +1,33 @@
+const homework65 = (function () {
+    function newBankAccount1() {
+        return {
+            ballance: 0,
+            performTransaction: function (amount) {
+                console.log(this.ballance += amount);
+            }
+        };
+    };
+    const myBankAccount1 = newBankAccount1();
+    const myBankAccount2 = newBankAccount1();
+
+    myBankAccount1.performTransaction(100);
+    myBankAccount2.performTransaction(50);
+    //hw step 2
+    function transaction(amount) {
+        console.log(this.ballance += amount);
+    };
+    function newBankAccount2() {
+        return {
+            ballance: 0,
+        };
+    };
+    const myBankAccount3 = newBankAccount2();
+    const myBankAccount4 = newBankAccount2();
+    transaction.call(myBankAccount3, 150);
+    transaction.call(myBankAccount3, 25);
+    transaction.call(myBankAccount4, 25);
+    //hw step 3
+    const depositFiftyinSavings = transaction.bind(myBankAccount3, 50);
+    depositFiftyinSavings();
+
+}());
