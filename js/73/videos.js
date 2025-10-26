271c872a1f926b1ba112f8fa8e72271d25a7e60a
samuelhirsch <shmielhirsch@gmail.com>
2025-10-25 23:30:10 -0400

    hw(73)

--- js/73/videos.js
+++ js/73/videos.js
@@ -3,28 +3,28 @@
   const myVideos = document.querySelector('#videolist');
   try {
     const response = await fetch('videos.json');
-   if(!response.ok){
-    throw new Error(`${response.status}-${response.statusText}`);
-   }
+    if (!response.ok) {
+      throw new Error(`${response.status}-${response.statusText}`);
+    }
     const videosList = await response.json();
     videosList.forEach(video => {
       const newLi = document.createElement('li');
       const title = document.createElement('header');
       title.innerText = video.title;
       const newVideo = document.createElement('video');
-    newLi.addEventListener('click',()=>{
-       newVideo.play();
-    });
+      newLi.addEventListener('click', () => {
+        newVideo.play();
+      });
       newVideo.src = video.url;
-      if(video.image){
+      if (video.image) {
         newVideo.poster = video.image;
       }
-      else{
-        newVideo.poster=('images/music.png');
+      else {
+        newVideo.poster = ('images/music.png');
       }
       newVideo.controls = true;
       newLi.appendChild(newVideo);
-       newLi.appendChild(title);
+      newLi.appendChild(title);
       myVideos.appendChild(newLi);
     });
   }
